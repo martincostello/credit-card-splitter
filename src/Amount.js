d208542390f13486e95a36910fe114b430a443bd
martincostello <martin@martincostello.com>
2017-08-26 12:11:22 +0100

    Fix handling of zero for amounts
    Fix issues with the Amount control when the value was 0, such as
    entering 0 to start entering a value in pence (e.g. 0.20).

--- src/Amount.js
+++ src/Amount.js
@@ -13,11 +13,23 @@ class Amount extends Component {
   }
 
   handleChange(event) {
+
+    var value = event.target.value;
+
+    if (!value) {
+      this.setState({ value: parsed });
+      return;
+    }
+
     var parsed = parseFloat(event.target.value);
-    if (parsed && !isNaN(parsed) && parsed >= this.props.min) {
+
+    if (parsed === 0) {
+      this.setState({ value: parsed });
+    }
+    else if (parsed && !isNaN(parsed) && parsed >= this.props.min) {
       var split = event.target.value.split(".");
       if (split.length < 2 || split[1].length < 3) {
-        this.setState({value: parsed});
+        this.setState({ value: parsed });
         if (this.props.onValueChanged) {
           this.props.onValueChanged(parsed);
         }
@@ -39,18 +51,18 @@ class Amount extends Component {
       <div className="input-group">
         <span className="input-group-addon" aria-hidden="true">{this.props.currency || "£"}</span>
         <input name={this.props.name || ""}
-               className="form-control"
-               placeholder={this.props.placeholder || "0.00"}
-               min={this.props.min || "0.00"}
-               max={this.props.max || ""}
-               step="0.01"
-               type="number"
-               disabled={!this.state.canEdit}
-               value={this.state.value || ""}
-               onChange={this.handleChange}
-               onKeyPress={this.onKeyPress}
-               aria-label={this.props.label || ""}
-               label={this.props.label || ""} />
+          className="form-control"
+          placeholder={this.props.placeholder || "0.00"}
+          min={this.props.min || "0.00"}
+          max={this.props.max || ""}
+          step="0.01"
+          type="number"
+          disabled={!this.state.canEdit}
+          value={this.state.value}
+          onChange={this.handleChange}
+          onKeyPress={this.onKeyPress}
+          aria-label={this.props.label || ""}
+          label={this.props.label || ""} />
       </div>
     );
   }
