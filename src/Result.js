e4dd7e645fb20c0354d4e753d5f75f16b1f9aa21
martincostello <martin@martincostello.com>
2017-08-26 16:25:53 +0100

    Add share button to modal
    Add the share button to a modal.
    Also add a link for if the clipboard copy does not work.

--- src/Result.js
+++ src/Result.js
@@ -1,3 +1,4 @@
+import Clipboard from "clipboard";
 import React, { Component } from "react";
 
 class Result extends Component {
@@ -12,19 +13,31 @@ class Result extends Component {
     this.total = props.total;
     this.total1 = props.total1;
     this.total2 = props.total2;
+    this.clipboard = null;
   }
 
-  buildShareHash() {
+  buildShareUrl() {
     var json = JSON.stringify({
-          currency: this.currency,
-          person1: this.person1,
-          person2: this.person2,
-          total: this.total,
-          total1: this.total1,
-          total2: this.total2
-        });
+        currency: this.currency,
+        person1: this.person1,
+        person2: this.person2,
+        total: this.total,
+        total1: this.total1,
+        total2: this.total2
+    });
     var encoded = btoa(json);
-    return "#" + encodeURIComponent(encoded);
+
+    var shareUrl = window.location.href;
+
+    if (shareUrl[shareUrl.length - 1] === '#') {
+        shareUrl = shareUrl.substring(0, shareUrl.length - 1);
+    }
+
+    return shareUrl + "#" + encodeURIComponent(encoded);
+  }
+
+  componentDidMount() {
+      setTimeout(() => this.clipboard = new Clipboard(".btn-copy"), 500);
   }
 
   render() {
@@ -37,11 +50,40 @@ class Result extends Component {
           <p>
             {this.person2}: <strong>{this.currency}{this.total2}</strong>
           </p>
-          { this.showShareButton ? <p>
-                <a className="btn btn-large btn-primary" href={this.buildShareHash()} target="_blank">
-                    Share <span className="glyphicon glyphicon-share" aria-hidden="true"></span>
-                </a>
-            </p> : ""
+          { this.showShareButton ? <div>
+                <p>
+                    <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#share-modal">
+                        Share <span className="glyphicon glyphicon-share" aria-hidden="true"></span>
+                    </button>
+                </p>
+                <div className="modal fade" id="share-modal" tabIndex="-1" role="dialog" aria-labelledby="share-modal-label">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 className="modal-title" id="share-modal-label">Share</h4>
+                            </div>
+                            <div className="modal-body">
+                                <p className="lead">
+                                    Click the button below to copy a URL to use to share the result.
+                                </p>
+                                <div className="btn-toolbar" role="toolbar">
+                                    <div className="btn-group" role="group">
+                                        <button className="btn btn-primary btn-copy" data-clipboard-action="copy" data-clipboard-text={this.buildShareUrl()}>
+                                            Copy to clipboard <span className="glyphicon glyphicon-copy" aria-hidden="true"></span>
+                                        </button>
+                                    </div>
+                                    <div className="btn-group" role="group">
+                                        <a className="btn btn-default" href={this.buildShareUrl()} target="_blank">
+                                            View result <span className="glyphicon glyphicon-share" aria-hidden="true"></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> : ""
           }
         </div>
     );
