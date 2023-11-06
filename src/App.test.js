a0d7b3ce8367220e0ee09182acd55f26e952f0a5
martincostello <martin@martincostello.com>
2023-11-06 13:18:46 +0000

    Tidy up
    - Remove Travis CI.
    - Update React test for v18.
    - Update lock file.
    - Update browserslist.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  const container = document.createElement("div");
+  const root = createRoot(container);
+  root.render(<App />);
 });
